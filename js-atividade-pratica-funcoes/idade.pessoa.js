10f74031f4040404f04fbc4ca9e7456160fd1989
Ramias Souza <ramias@inovadora.com.br>
2021-10-25 14:39:04 -0300

    Finalização do módulo de funções - JS

---
+++ js-atividade-pratica-funcoes/idade.pessoa.js
@@ -0,0 +1,23 @@
+function calculaIdade(anos){
+   return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
+}
+
+const pessoa1 = {
+   nome: 'Maria',
+   idade: 30
+};
+
+const pessoa2 = {
+   nome: 'Carla',
+   idade: 26
+};
+
+const animal = {
+   nome: 'Fiona',
+   idade: 5,
+   raca: 'Pug'
+};
+
+console.log(calculaIdade.call(pessoa1, 20)); // Método call
+
+console.log(calculaIdade.apply(pessoa2, [25]));
