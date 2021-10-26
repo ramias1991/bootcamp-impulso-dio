9023398274c80ebb47cd974f3006c4c40b7ae7c9
Ramias Souza <ramias@inovadora.com.br>
2021-10-26 14:12:54 -0300

    Finalização módulo Error -JS

---
+++ js-atividade-pratica-debugging-errors/index.js
@@ -0,0 +1,30 @@
+function validateArray(arr, num){
+   try{
+      if(!arr && !num) throw new ReferenceError("Envie os parâmetros");
+
+      if(typeof arr !== 'object') 
+         throw new TypeError("Foi recebido um array ao invés de um objeto");
+
+      if(typeof num !== 'number')
+         throw new TypeError("Precisa ser um number");
+
+      if(arr.length !== num) throw new RangeError("Tamanho inválido");
+
+      return arr;
+   } catch(e){
+      if(e instanceof ReferenceError){
+         console.log('Este erro é um ReferenceError')
+         console.log(e.message)
+      } else if(e instanceof TypeError) {
+         console.log('Este erro é um TypeError')
+         console.log(e.message)
+      } else if(e instanceof RangeError) {
+         console.log('Este erro é um RangeError')
+         console.log(e.message)
+      } else {
+         console.log('Tipo de erro não esperado: ' + e);
+      }
+   }
+}
+
+console.log(validateArray([1,2,3,4,5],5));
---
+++ js-atividade-pratica-modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby, nome } from "./funcoes.mjs";
+
+console.log(mostraIdade(nome, 30));
+console.log(mostraCidade(nome, 'Joaçaba SC'));
+console.log(mostraHobby(nome, 'Tocar violão'));
